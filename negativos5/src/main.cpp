4a8055172f80f1ab5460257aa4c95e4f66a580ae
Flaviano <flavianoventuras@hotmail.com>
2021-10-28 15:34:39 -0300

    Update main.cpp

--- negativos5/src/main.cpp
+++ negativos5/src/main.cpp
@@ -7,6 +7,15 @@ const int SIZE = 5; // input size.
 int main(void)
 {
     // TODO: Adicione aqui seu código.
+    int num;
+    int qtd = 0;
+    for(int i = 1; i<=SIZE; i++){
+        cin>>num;
+           if(num < 0){
+               qtd++;
+           }
+    }
 
+    cout << qtd <<std::endl;
     return 0;
 }
