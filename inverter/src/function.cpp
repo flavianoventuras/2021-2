5e1fc04184d2c0f110bb05113d3614c1d8fd06d3
Flaviano <flavianoventuras@hotmail.com>
2021-11-03 16:32:41 -0300

    Update function.cpp

--- inverter/src/function.cpp
+++ inverter/src/function.cpp
@@ -17,4 +17,27 @@ int main() {
     cout << endl;
 
     return EXIT_SUCCESS;
+    
+    /*
+    #include <iostream>
+#include <iomanip>
+#include <vector>
+
+using std::cout; using std::endl;
+using std::vector; using std::fixed;
+using std::setprecision;
+
+int main() {
+    vector<double> d_vec = {123.231, 2.2343, 0.324, 0.012,
+                            26.9491092019, 11013,
+                            92.001112, 0.000000234};
+
+    for (auto &i : d_vec) {
+        cout << fixed << setprecision(3) << i << " | ";
+    }
+    cout << endl;
+
+    return EXIT_SUCCESS;
+}
+    */
 }
