32a8a0076a41899e2d25bfbbf55ddea5eced4115
Flaviano <flavianoventuras@hotmail.com>
2021-11-02 22:24:46 -0300

    Update main.cpp

--- intervalos/src/main.cpp
+++ intervalos/src/main.cpp
@@ -17,6 +17,7 @@ int main(void)
 {
     int numero, qtd;
     int grupo1 = 0, grupo2 = 0, grupo3 = 0, grupo4 = 0, grupo5 = 0;
+    float porcen1,porcen2,porcen3,porcen4,porcen5;
     cout<<"Digite a quantidade de números: ";
     cin >> qtd;
 
@@ -24,19 +25,28 @@ int main(void)
         cout<<"Digite um numero:"<<endl;
         cin >> numero;
         if(numero > 0 && numero < 25){
-
-        }else if (numero >= 25 && numero < 50){
             grupo1++;
+        }else if (numero >= 25 && numero < 50){
+            grupo2++;
         }else if (numero >= 50 && numero < 75){
-            
+            grupo3++;
         }else if (numero >= 75 && numero < 100){
-            
+            grupo4++;
         }else {
-            
+            grupo5++;
         }     
     }
 
-    cout<<"Grupo 1: " << grupo1 << endl;
+    porcen1 = (grupo1*100)/qtd;
+    porcen2 = (grupo2*100)/qtd;
+    porcen3 = (grupo3*100)/qtd;
+    porcen4 = (grupo4*100)/qtd;
+    porcen5 = (grupo5*100)/qtd;
+    cout<<"Grupo 1 obteve " << porcen1 << "%" <<endl;
+    cout<<"Grupo 2 obteve " << porcen2 << "%" <<endl;
+    cout<<"Grupo 3 obteve " << porcen3 << "%" <<endl;
+    cout<<"Grupo 4 obteve " << porcen4 << "%" <<endl;
+    cout<<"Grupo 5 obteve " << porcen5 << "%" <<endl;
 
     return 0;
 }
