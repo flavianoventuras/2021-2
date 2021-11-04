745a39174d63fddf87453a5b5dace49871149f7a
Flaviano <flavianoventuras@hotmail.com>
2021-11-04 16:46:05 -0300

    Update main.cpp

--- intervalos/src/main.cpp
+++ intervalos/src/main.cpp
@@ -13,35 +13,49 @@ using std::setprecision;
 
 // Se desejar, crie funções aqui, antes do main().
 
-int main(void)
+int main(int argc, char * argv[])
 {
     int numero, qtd;
-    int grupo1 = 0, grupo2 = 0, grupo3 = 0, grupo4 = 0, grupo5 = 0;
+    //int grupo1 = 0, grupo2 = 0, grupo3 = 0, grupo4 = 0, grupo5 = 0;
     float porcen1,porcen2,porcen3,porcen4,porcen5;
     cout<<"Digite a quantidade de números: ";
     cin >> qtd;
+    int vetor[qtd];
 
-    for(int i = 1; i<=qtd; i++){
+    for(int i = 0; i<qtd; i++){
         cout<<"Digite um numero:"<<endl;
         cin >> numero;
-        if(numero > 0 && numero < 25){
-            grupo1++;
-        }else if (numero >= 25 && numero < 50){
-            grupo2++;
-        }else if (numero >= 50 && numero < 75){
-            grupo3++;
-        }else if (numero >= 75 && numero < 100){
-            grupo4++;
-        }else {
-            grupo5++;
-        }     
+        vetor[i] = numero   ;
+
     }
 
-    porcen1 = (grupo1*100)/qtd;
-    porcen2 = (grupo2*100)/qtd;
-    porcen3 = (grupo3*100)/qtd;
-    porcen4 = (grupo4*100)/qtd;
-    porcen5 = (grupo5*100)/qtd;
+    for(int i = 0; i<qtd; i++){
+        cout << vetor[i] << endl;
+    }
+
+    
+    for(int i = 0; i < qtd; i++){
+        if(numero > 0 && numero < 25){
+            vetor[i]++;
+        }else if (numero >= 25 && numero < 50){
+           // grupo2++;
+        }else if (numero >= 50 && numero < 75){
+           // grupo3++;
+        }else if (numero >= 75 && numero < 100){
+           // grupo4++;
+        }else {
+           //    grupo5++;
+        }     
+    }
+    for(int i = 0; i < 0; i++){
+
+    }
+
+    porcen1 = (vetor[0]*100)/qtd;
+    porcen2 = (vetor[1]*100)/qtd;
+    porcen3 = (vetor[2]*100)/qtd;
+    porcen4 = (vetor[3]*100)/qtd;
+    
     cout<<"Grupo 1 obteve " << porcen1 << "%" <<endl;
     cout<<"Grupo 2 obteve " << porcen2 << "%" <<endl;
     cout<<"Grupo 3 obteve " << porcen3 << "%" <<endl;
